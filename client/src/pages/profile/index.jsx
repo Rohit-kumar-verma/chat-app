849bf9a9e089baee85ff65f95625cf94c8ca6d13
Rohit Kumar <rohitverma7754@gmail.com>
2024-11-08 20:40:26 +0530

    new commit

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppStore } from '../../store'
+import {IoArrowBack} from 'react-icons/io5'
 
 function Profile() {
-  const {userInfo} = useAppStore( )
+  const {userInfo} = useAppStore()
+  const [firstName, setFirstName]= useState("")
+  const [lastName, setLastName]= useState("")
+  const [image, setImage]= useState(null)
+  const [hovered, setHovered]= useState(false)
+  const [selectedColor, setSelectedColor]= useState(0)
+
   return (
-    <div>
-        Profile
-        <div>Email:{userInfo.id}</div>
+    <div className='bg-[#1b1c24] h-[100vh] flex items-center flex-col justify-center gap-10'>
+        <div className='flex flex-col w-[80vw] md:w-max gap-10'>
+          <IoArrowBack/>
+        </div>
     </div>
   )
 }
